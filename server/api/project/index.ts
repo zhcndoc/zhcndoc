aae579c359ebc0b42147b2b0e0a20a6362b7f81b
一纸忘忧 <i@ikxin.com>
2024-12-19 21:23:51 +0800

    refactor: replace event handler with cached event handler

--- server/api/project/index.ts
+++ server/api/project/index.ts
@@ -1,24 +1,31 @@
-export default defineEventHandler(async () => {
-  const ignoreRepos = ["zhcndoc"];
+export default defineCachedEventHandler(
+  async () => {
+    const ignoreRepos = ["zhcndoc"];
 
-  // 使用 octokit 调用 API
-  const { data } = await octokit.request("GET /orgs/{org}/repos", {
-    org: "zhcndoc",
-  });
+    // 使用 octokit 调用 API
+    const { data } = await octokit.request("GET /orgs/{org}/repos", {
+      org: "zhcndoc",
+    });
 
-  // 过滤忽略的项目
-  const filteredData = data.filter((repo) => !ignoreRepos.includes(repo.name));
+    // 过滤忽略的项目
+    const filteredData = data.filter(
+      (repo) => !ignoreRepos.includes(repo.name)
+    );
 
-  // 只返回指定字段
-  const selectedData = filteredData.map((repo) => {
-    return {
-      name: repo.name,
-      description: repo.description,
-      homepage: repo.homepage,
-      created_at: repo.created_at,
-      updated_at: repo.updated_at,
-    }
-  });
+    // 只返回指定字段
+    const selectedData = filteredData.map((repo) => {
+      return {
+        name: repo.name,
+        description: repo.description,
+        homepage: repo.homepage,
+        created_at: repo.created_at,
+        updated_at: repo.updated_at,
+      };
+    });
 
-  return selectedData;
-});
+    return selectedData;
+  },
+  {
+    maxAge: 60 * 60,
+  }
+);
