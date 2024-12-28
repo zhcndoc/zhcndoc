9e51475b83325443f42f14553f80bca2f551c3e8
一纸忘忧 <i@ikxin.com>
2024-12-28 22:30:37 +0800

    refactor: simplify event handler in compare.ts

--- server/api/project/compare.ts
+++ server/api/project/compare.ts
@@ -1,28 +1,23 @@
 import projects from "../../../data/projects.json";
 
-export default defineCachedEventHandler(
-  async (event) => {
-    const { repo } = getQuery(event);
+export default defineEventHandler(async (event) => {
+  const { repo } = getQuery(event);
 
-    if (repo && projects.find((p) => p.name === String(repo))) {
-      // 使用 octokit 调用 API
-      const { data } = await octokit.request(
-        "GET /repos/{owner}/{repo}/compare/{base}...{head}",
-        {
-          owner: "zhcndoc",
-          repo: String(repo),
-          base: "main",
-          head: "upstream",
-        }
-      );
+  if (repo && projects.find((p) => p.name === String(repo))) {
+    // 使用 octokit 调用 API
+    const { data } = await octokit.request(
+      "GET /repos/{owner}/{repo}/compare/{base}...{head}",
+      {
+        owner: "zhcndoc",
+        repo: String(repo),
+        base: "main",
+        head: "upstream",
+      }
+    );
 
-      // 只返回指定字段
-      return {
-        ahead_by: data?.ahead_by,
-      };
-    }
-  },
-  {
-    maxAge: 60,
+    // 只返回指定字段
+    return {
+      ahead_by: data?.ahead_by,
+    };
   }
-);
+});
