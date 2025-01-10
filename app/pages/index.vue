1f80c42bad4ac5c918c9ec3a3075db30691c0c7e
一纸忘忧 <i@ikxin.com>
2025-01-10 17:28:26 +0800

    feat: add ProjectTable component and enhance index page layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,1 +1,22 @@
-<template></template>
+<script setup lang="ts"></script>
+
+<template>
+  <div class="relative">
+    <UPageHero orientation="vertical">
+      <template #title>
+        <div class="flex flex-col gap-4 font-serif">
+          <span>由开源社区提供支持的</span>
+          <span class="text-[#ff3f57]">中文开发文档</span>
+        </div>
+      </template>
+      <template #description>
+        <div class="my-6">旨在为简体中文地区开发者提供优质的文档查阅体验</div>
+      </template>
+      <template #links>
+        <UButton to="/project" variant="soft" color="neutral" size="xl">
+          查看项目
+        </UButton>
+      </template>
+    </UPageHero>
+  </div>
+</template>
