ed174e66e5c76b30c0bc483b79a9b05346e8542b
一纸忘忧 <i@ikxin.com>
2025-01-31 15:57:37 +0800

    refactor: update umami script source

--- public/js/common.js
+++ public/js/common.js
@@ -1,26 +1,23 @@
 function loadUmami() {
-  const script = document.createElement("script");
-  script.defer = true;
-  script.src = "https://analytics.ikxin.com/script.js";
-  script.setAttribute(
-    "data-website-id",
-    "f0e90b0d-e086-4fdc-b173-de4857b71900"
-  );
-  document.head.appendChild(script);
+  const script = document.createElement('script')
+  script.defer = true
+  script.src = 'https://umami.ikxin.com/script.js'
+  script.setAttribute('data-website-id', 'f0e90b0d-e086-4fdc-b173-de4857b71900')
+  document.head.appendChild(script)
 }
 
 function loadGoogleAnalytics() {
-  const gtagScript = document.createElement("script");
-  gtagScript.async = true;
-  gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-HYH4TH7PWM";
-  document.head.appendChild(gtagScript);
-  window.dataLayer = window.dataLayer || [];
+  const gtagScript = document.createElement('script')
+  gtagScript.async = true
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-HYH4TH7PWM'
+  document.head.appendChild(gtagScript)
+  window.dataLayer = window.dataLayer || []
   function gtag() {
-    dataLayer.push(arguments);
+    dataLayer.push(arguments)
   }
-  gtag("js", new Date());
-  gtag("config", "G-HYH4TH7PWM");
+  gtag('js', new Date())
+  gtag('config', 'G-HYH4TH7PWM')
 }
 
-loadUmami();
+loadUmami()
 // loadGoogleAnalytics();
