97cae88844134db1ec7e99dacf6a85e60fe9109b
一纸忘忧 <i@ikxin.com>
2025-02-24 23:22:53 +0800

    chore: remove projects.json and fetch-github utility

--- server/utils/fetch-github.ts
+++
@@ -1,8 +0,0 @@
-export const fetchGithub = <T>(url: string): Promise<T> => {
-  const baseUrl = "https://api.github.com";
-  const headers = {
-    Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-  };
-
-  return $fetch(`${baseUrl}${url}`, { headers });
-};
