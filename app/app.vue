33d2dd9134b78ceb06f787c689828093168551d4
一纸忘忧 <i@ikxin.com>
2025-02-25 11:09:00 +0800

    feat: add website icon and manifest files

--- app/app.vue
+++ app/app.vue
@@ -8,6 +8,13 @@ useHead({
         '由开源社区提供支持的中文技术文档，旨在为中文地区开发者提供优质的技术文档查阅体验。',
     },
   ],
+  link: [
+    {
+      href: '/site.webmanifest',
+      rel: 'manifest',
+      crossorigin: 'use-credentials',
+    },
+  ],
   script: [{ src: '/js/common.js', async: true }],
 })
 
