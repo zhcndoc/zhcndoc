f89218e70f4d68b728511749bbce769dd4ae4723
一纸忘忧 <i@ikxin.com>
2025-03-13 19:49:14 +0800

    refactor: update common script config

--- app/app.vue
+++ app/app.vue
@@ -7,10 +7,6 @@ useHead({
       content:
         '由开源社区提供支持的中文技术文档，旨在为中文地区开发者提供优质的技术文档查阅体验。',
     },
-    {
-      name: 'wwads-cn-verify',
-      content: 'f74909ace68e51891440e4da0b65a70c',
-    },
   ],
   link: [
     {
@@ -21,10 +17,6 @@ useHead({
   ],
   script: [{ src: '/js/common.js', async: true }],
 })
-
-// const projects = useState<GithubRepo[]>('projects', () => [])
-
-// projects.value = await $fetch<GithubRepo[]>('/api/project')
 </script>
 
 <template>
--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,12 @@
+var _hmt = _hmt || []
+
+function loadBaiduAnalytics() {
+  var hm = document.createElement('script')
+  hm.src = 'https://hm.baidu.com/hm.js?8952f7167d0a961aeccca9dba7df5781'
+  var s = document.getElementsByTagName('script')[0]
+  s.parentNode.insertBefore(hm, s)
+}
+
 function loadUmami() {
   const script = document.createElement('script')
   script.defer = true
@@ -6,18 +15,15 @@ function loadUmami() {
   document.head.appendChild(script)
 }
 
-function loadGoogleAnalytics() {
-  const gtagScript = document.createElement('script')
-  gtagScript.async = true
-  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-HYH4TH7PWM'
-  document.head.appendChild(gtagScript)
-  window.dataLayer = window.dataLayer || []
-  function gtag() {
-    dataLayer.push(arguments)
-  }
-  gtag('js', new Date())
-  gtag('config', 'G-HYH4TH7PWM')
+function loadWwads() {
+  const script = document.createElement('script')
+  script.type = 'text/javascript'
+  script.charset = 'UTF-8'
+  script.src = 'https://cdn.wwads.cn/js/makemoney.js'
+  script.async = true
+  document.head.appendChild(script)
 }
 
+loadBaiduAnalytics()
 loadUmami()
-// loadGoogleAnalytics();
+loadWwads()
