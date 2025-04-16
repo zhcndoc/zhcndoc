2c4ed3a752446dbcc8cd75ad1d9412f10d5aa461
一纸忘忧 <i@ikxin.com>
2025-04-16 10:11:00 +0800

    feat: enhance navigation menu with dynamic routing

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,19 +1,54 @@
 <script lang="ts" setup>
-const items = [
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const route = useRoute()
+
+const items = computed<NavigationMenuItem[]>(() => [
   {
     label: '项目',
     icon: 'tabler:list-details',
     to: '/projects',
+    active: route.path.startsWith('/projects'),
+    children: [
+      {
+        label: '探索',
+        icon: 'tabler:search',
+        to: '/projects',
+        description: '全面探索各类中文文档项目，发现更多优质资源',
+      },
+      {
+        label: '概览',
+        icon: 'tabler:layout-dashboard',
+        to: '/projects/overview',
+        description: '查看中文文档项目在 Github 仓库的整体概览情况',
+      },
+    ],
   },
-]
+  {
+    label: '统计',
+    icon: 'tabler:chart-pie',
+    to: '/analytics',
+    description: '深入了解各项数据的统计信息，掌握业务动态',
+    active: route.path.startsWith('/analytics'),
+  },
+])
 </script>
 
 <template>
-  <UHeader title="简中文档" class="fixed top-0 w-full">
+  <UHeader
+    title="简中文档"
+    class="fixed top-0 w-full"
+    :ui="{
+      center: 'flex-2',
+    }"
+  >
     <template #title>
       <Logo />
     </template>
-    <UNavigationMenu :items="items" />
+    <UNavigationMenu :items="items" class="w-full justify-center" />
+    <template #body>
+      <UNavigationMenu :items="items" orientation="vertical" class="-mx-2.5" />
+    </template>
     <template #right>
       <UColorModeButton>
         <template #fallback>
