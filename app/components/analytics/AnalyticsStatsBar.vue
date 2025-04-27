fe23babbdf241931c3975b84611ab297e0380470
一纸忘忧 <i@ikxin.com>
2025-04-27 17:28:39 +0800

    fix: metrics computation with conditional check

--- app/components/analytics/AnalyticsStatsBar.vue
+++ app/components/analytics/AnalyticsStatsBar.vue
@@ -9,55 +9,61 @@ const { data, status } = await useFetch('/api/analytics/stats', {
   },
 })
 
-const metrics = computed(() => [
-  {
-    ...data.value?.pageviews,
-    label: '浏览量',
-    change: data.value?.pageviews.value - data.value?.pageviews.prev,
-    formatValue: formatLongNumber,
-  },
-  {
-    ...data.value?.visits,
-    label: '访问次数',
-    change: data.value?.visits.value - data.value?.visits.prev,
-    formatValue: formatLongNumber,
-  },
-  {
-    ...data.value?.visitors,
-    label: '访客',
-    change: data.value?.visitors.value - data.value?.visitors.prev,
-    formatValue: formatLongNumber,
-  },
-  {
-    label: '跳出率',
-    value:
-      (Math.min(data.value?.visits.value, data.value?.bounces.value) /
-        data.value?.visits.value) *
-      100,
-    prev:
-      (Math.min(data.value?.visits.prev, data.value?.bounces.prev) /
-        data.value?.visits.prev) *
-      100,
-    change:
-      (Math.min(data.value?.visits.value, data.value?.bounces.value) /
-        data.value?.visits.value) *
-        100 -
-      (Math.min(data.value?.visits.prev, data.value?.bounces.prev) /
-        data.value?.visits.prev) *
-        100,
-    formatValue: (value: number) => Math.round(+value) + '%',
-    reverseColors: true,
-  },
-  {
-    label: '平均时间',
-    value: data.value?.totaltime.value / data.value?.visits.value,
-    prev: data.value?.totaltime.prev / data.value?.visits.prev,
-    change:
-      data.value?.totaltime.value / data.value?.visits.value -
-      data.value?.totaltime.prev / data.value?.visits.prev,
-    formatValue: formatSeconds,
-  },
-])
+const metrics = computed(() => {
+  if (data.value) {
+    return [
+      {
+        ...data.value?.pageviews,
+        label: '浏览量',
+        change: data.value?.pageviews.value - data.value?.pageviews.prev,
+        formatValue: formatLongNumber,
+      },
+      {
+        ...data.value?.visits,
+        label: '访问次数',
+        change: data.value?.visits.value - data.value?.visits.prev,
+        formatValue: formatLongNumber,
+      },
+      {
+        ...data.value?.visitors,
+        label: '访客',
+        change: data.value?.visitors.value - data.value?.visitors.prev,
+        formatValue: formatLongNumber,
+      },
+      {
+        label: '跳出率',
+        value:
+          (Math.min(data.value?.visits.value, data.value?.bounces.value) /
+            data.value?.visits.value) *
+          100,
+        prev:
+          (Math.min(data.value?.visits.prev, data.value?.bounces.prev) /
+            data.value?.visits.prev) *
+          100,
+        change:
+          (Math.min(data.value?.visits.value, data.value?.bounces.value) /
+            data.value?.visits.value) *
+            100 -
+          (Math.min(data.value?.visits.prev, data.value?.bounces.prev) /
+            data.value?.visits.prev) *
+            100,
+        formatValue: (value: number) => Math.round(+value) + '%',
+        reverseColors: true,
+      },
+      {
+        label: '平均时间',
+        value: data.value?.totaltime.value / data.value?.visits.value,
+        prev: data.value?.totaltime.prev / data.value?.visits.prev,
+        change:
+          data.value?.totaltime.value / data.value?.visits.value -
+          data.value?.totaltime.prev / data.value?.visits.prev,
+        formatValue: formatSeconds,
+      },
+    ]
+  } else {
+    return []
+  }
+})
 </script>
 
 <template>
