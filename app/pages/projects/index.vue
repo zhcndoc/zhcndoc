ba5df0c9bd52668e057d3cc6258b284d465a2f8e
一纸忘忧 <i@ikxin.com>
2025-04-28 16:49:22 +0800

    chore: update project descriptions for clarity and accuracy

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -16,19 +16,17 @@ const { data: projects } = await useFetch<ProjectInfo[]>('/api/projects', {
           :ui="{
             container: 'p-3 sm:p-4',
           }"
-          spotlight
-          spotlight-color="primary"
+          @click="navigateTo(`/projects/${project.name}`)"
         >
           <div class="flex flex-col">
-            <NuxtLink
-              class="flex cursor-pointer gap-4"
-              :to="`/projects/${project.name}`"
-            >
+            <div class="flex gap-4">
               <div class="h-20 min-w-20 overflow-hidden rounded-xl">
-                <img
-                  :src="`/images/projects/${project.name}.svg`"
-                  class="h-full w-full object-cover object-center"
-                />
+                <NuxtLink :to="`/projects/${project.name}`">
+                  <img
+                    :src="`/images/projects/${project.name}.svg`"
+                    class="h-full w-full object-cover object-center"
+                  />
+                </NuxtLink>
               </div>
               <div class="flex min-w-0 flex-1 flex-col gap-2">
                 <h2 class="font-sans text-lg font-semibold">
@@ -38,12 +36,13 @@ const { data: projects } = await useFetch<ProjectInfo[]>('/api/projects', {
                   {{ project.description }}
                 </span>
               </div>
-            </NuxtLink>
+            </div>
             <div class="mt-3 flex justify-between">
-              <div class="flex gap-4 text-sm text-gray-500">
+              <div class="flex items-center gap-4 text-sm text-gray-500">
                 <NuxtLink
-                  :to="`https://github.com/zhcndoc/${project.name}/stargazers`"
                   target="_blank"
+                  :to="`https://github.com/zhcndoc/${project.name}/stargazers`"
+                  @click.stop
                 >
                   <div class="flex items-center gap-1">
                     <UIcon name="tabler:star" />
@@ -51,8 +50,9 @@ const { data: projects } = await useFetch<ProjectInfo[]>('/api/projects', {
                   </div>
                 </NuxtLink>
                 <NuxtLink
-                  :to="`https://github.com/zhcndoc/${project.name}/forks`"
                   target="_blank"
+                  :to="`https://github.com/zhcndoc/${project.name}/forks`"
+                  @click.stop
                 >
                   <div class="flex items-center gap-1">
                     <UIcon name="tabler:git-fork" />
@@ -67,6 +67,7 @@ const { data: projects } = await useFetch<ProjectInfo[]>('/api/projects', {
                   target="_blank"
                   variant="soft"
                   color="primary"
+                  @click.stop
                 />
                 <UButton
                   label="官方文档"
@@ -74,6 +75,7 @@ const { data: projects } = await useFetch<ProjectInfo[]>('/api/projects', {
                   target="_blank"
                   variant="soft"
                   color="neutral"
+                  @click.stop
                 />
               </div>
             </div>
