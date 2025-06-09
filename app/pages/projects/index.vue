d5920d1e8fd7d29db37cedcfb4f14201d47bf857
Liang <liang@in-x.cc>
2025-06-09 20:35:03 +0800

    fix: clicking on a card to jump to an unfinished page (#12)

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -16,18 +16,15 @@ const { data: projects } = await useFetch<ProjectInfo[]>('/api/projects', {
           :ui="{
             container: 'p-3 sm:p-4',
           }"
-          @click="navigateTo(`/projects/${project.name}`)"
         >
           <div class="flex flex-col">
             <div class="flex gap-4">
               <div class="h-20 min-w-20 overflow-hidden rounded-xl">
-                <NuxtLink :to="`/projects/${project.name}`">
                   <img
                     :src="`/images/projects/${project.name}.svg`"
                     class="bg-elevated h-full w-full object-cover object-center"
                     loading="lazy"
                   />
-                </NuxtLink>
               </div>
               <div class="flex min-w-0 flex-1 flex-col gap-2">
                 <h2 class="font-sans text-lg font-semibold">
