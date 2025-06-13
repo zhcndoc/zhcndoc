57e36240b48ab19d176d8fe0300bb27e100dbaa1
一纸忘忧 <i@ikxin.com>
2025-06-13 14:40:26 +0800

    feat: add zod and nuxt-content project configuration

--- app/pages/projects/[slug].vue
+++ app/pages/projects/[slug].vue
@@ -1,5 +1,10 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const route = useRoute()
+const slug = route.params.slug as string
+</script>
 
 <template>
-  <UContainer class="py-4"></UContainer>
+  <UContainer class="py-4">{{
+    slug ? `Project: ${slug}` : 'No project selected'
+  }}</UContainer>
 </template>
