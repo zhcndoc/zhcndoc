bc52f64c9c14572615f8979d65baaf48d47e6536
一纸忘忧 <54543761+ikxin@users.noreply.github.com>
2026-04-19 00:45:35 +0800

    chore(website): 协助 ChatGPT 润色博客文案

--- website/app/app.config.ts
+++ website/app/app.config.ts
@@ -1,5 +1,3 @@
-import type { AppConfigInput } from 'nuxt/schema'
-
 export default {
   ui: {
     colors: {
@@ -23,6 +21,28 @@ export default {
       dark: 'tabler:moon',
       light: 'tabler:sun-high',
     },
+    prose: {
+      blockquote: {
+        base: 'not-italic',
+      },
+      ul: {
+        base: 'my-4 list-none ps-0 sm:my-5 [&_ul]:mt-2 [&_ul]:ps-4 sm:[&_ul]:ps-5',
+      },
+      li: {
+        base: "relative ps-0 indent-4 leading-6 sm:leading-7 before:absolute before:start-0 before:top-[0.82em] before:size-1.5 before:-translate-y-1/2 before:rounded-full before:bg-muted before:content-[''] [&_code]:indent-0 [&>ul]:mb-0",
+      },
+      table: {
+        slots: {
+          base: 'min-w-full w-max',
+        },
+      },
+      th: {
+        base: 'whitespace-nowrap',
+      },
+      td: {
+        base: 'whitespace-nowrap',
+      },
+    },
     navigationMenu: {
       slots: {
         list: 'list-none',
@@ -31,8 +51,7 @@ export default {
     table: {
       slots: {
         th: 'whitespace-nowrap',
-        td: 'whitespace-nowrap',
       },
     },
   },
-} satisfies AppConfigInput
+}
